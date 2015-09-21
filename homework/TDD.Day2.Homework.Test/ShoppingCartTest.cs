89a3ee115ea832e341019c69795749f2281a4c29
blackie1019 <blackie1019@gmail.com>
2015-09-21 13:46:26 +0800

    Added expected Fee on TestCase name

--- homework/TDD.Day2.Homework.Test/ShoppingCartTest.cs
+++ homework/TDD.Day2.Homework.Test/ShoppingCartTest.cs
@@ -12,7 +12,7 @@ namespace TDD.Day2.Homework.Test
         }
 
         [TestMethod]
-        public void Buy_1_Book_Vol_2()
+        public void Buy_1_Book_Vol_2_Fee_Should_Be_100()
         {
             //Scenario: 第一集買了一本，其他都沒買，價格應為100*1=100元
 
@@ -26,7 +26,7 @@ namespace TDD.Day2.Homework.Test
         }
 
         [TestMethod]
-        public void Buy_2_Book_Vol_1_And_2()
+        public void Buy_2_Book_Vol_1_And_2_Fee_Should_Be_190()
         {
             //Scenario: 第一集買了一本，第二集也買了一本，價格應為100 * 2 * 0.95 = 190
 
@@ -40,7 +40,7 @@ namespace TDD.Day2.Homework.Test
         }
 
         [TestMethod]
-        public void Buy_3_Book_Vol_1_And_2_And_3()
+        public void Buy_3_Book_Vol_1_And_2_And_3_Fee_Should_Be_270()
         {
             //Scenario: 一二三集各買了一本，價格應為100 * 3 * 0.9 = 270
 
@@ -54,7 +54,7 @@ namespace TDD.Day2.Homework.Test
         }
 
         [TestMethod]
-        public void Buy_4_Book_Vol_1_And_2_And_3_And_4()
+        public void Buy_4_Book_Vol_1_And_2_And_3_And_4_Fee_Should_Be_320()
         {
             //Scenario: 一二三四集各買了一本，價格應為100 * 4 * 0.8 = 320
 
@@ -68,7 +68,7 @@ namespace TDD.Day2.Homework.Test
         }
 
         [TestMethod]
-        public void Buy_5_Book_Vol_1_And_2_And_3_And_4_And_5()
+        public void Buy_5_Book_Vol_1_And_2_And_3_And_4_And_5_Fee_Should_Be_375()
         {
             //Scenario: 一次買了整套，一二三四五集各買了一本，價格應為100 * 5 * 0.75 = 375
 
@@ -82,7 +82,7 @@ namespace TDD.Day2.Homework.Test
         }
 
         [TestMethod]
-        public void Buy_4_Book_Vol_1_And_2_And_3_And_3()
+        public void Buy_4_Book_Vol_1_And_2_And_3_And_3_Fee_Should_Be_370()
         {
             //Scenario: 一二集各買了一本，第三集買了兩本，價格應為100 * 3 * 0.9 + 100 = 370
 
@@ -96,7 +96,7 @@ namespace TDD.Day2.Homework.Test
         }
 
         [TestMethod]
-        public void Buy_5_Book_Vol_1_And_2_And_2_And_3_And_3()
+        public void Buy_5_Book_Vol_1_And_2_And_2_And_3_And_3_Fee_Should_Be_460()
         {
             //Scenario: 第一集買了一本，第二三集各買了兩本，價格應為100 * 3 * 0.9 + 100 * 2 * 0.95 = 460
 
@@ -114,13 +114,31 @@ namespace TDD.Day2.Homework.Test
         #region Advanced
 
         [TestMethod]
-        public void Buy_2_Book_Vol_1_And_1()
+        public void Buy_2_Book_Vol_1_And_1_Fee_Should_Be_200()
         {
+            //Scenario: 第一集買了兩本，價格應為100 * 2 * 1.0  = 200
+
+            //Given 第一集買了 2 本
+            //And 第二集買了 0 本
+            //And 第三集買了 0 本
+            //And 第四集買了 0 本
+            //And 第五集買了 0 本
+            //When 結帳
+            //Then 價格應為 200 元
         }
 
         [TestMethod]
-        public void Buy_3_Book_Vol_1_And_1_And_2()
+        public void Buy_3_Book_Vol_1_And_1_And_2_Fee_Should_Be_290()
         {
+            //Scenario: 第一集買了兩本，第二買了ㄧ本，價格應為100 * 1 * 1.0 + 100 * 2 * 0.95 = 290
+
+            //Given 第一集買了 2 本
+            //And 第二集買了 1 本
+            //And 第三集買了 0 本
+            //And 第四集買了 0 本
+            //And 第五集買了 0 本
+            //When 結帳
+            //Then 價格應為 290 元
         }
 
         #endregion Advanced
