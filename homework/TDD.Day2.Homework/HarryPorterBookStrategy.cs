d1e1f363efaf9b8e35fcb10b13133ac47d9e8161
blackie.tsai <blackie.tsai@xuenn.com>
2015-09-24 20:38:40 +0800

    Add SeparateBookGroup but failed TestCase

--- homework/TDD.Day2.Homework/HarryPorterBookStrategy.cs
+++ homework/TDD.Day2.Homework/HarryPorterBookStrategy.cs
@@ -8,9 +8,29 @@ namespace TDD.Day2.Homework
     {
         public decimal DiscountAmount(IList<Book> books)
         {
-            decimal result = books.Sum(book => book.Price);
+            // decimal result = books.Sum(book => book.Price);
+            decimal result = 0;
+            var bookGroups = this.SeparateBookGroup(books);
 
             return result;
+        }
+
+        private Dictionary<string, int> SeparateBookGroup(IList<Book> books)
+        {
+            var bookGroups = new Dictionary<string, int>();
+            foreach (var book in books)
+            {
+                if (bookGroups.Any(x => x.Key == book.BookType))
+                {
+                    var bookGroup = bookGroups.First(x => x.Key == book.BookType);
+                    bookGroups[book.BookType] = bookGroup.Value + 1;
+                }
+                else
+                {
+                    bookGroups.Add(book.BookType, 1);
+                }
+            }
+            return bookGroups;
         }
     }
 }
